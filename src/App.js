03618b12b256fd8c30b8336f3da33c76b2dc968c
Yauhen Klimovich <webrubin1987@gmail.com>
2022-11-27 22:03:16 +0300

    feat: add card html and css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
         <div className="wrapper">
             <header>
                 <div className="header-left-block">
-                    <img width={150} height={200} src="/img/logoMain.jpg"/>
+                    <img width={120} height={150} src="/img/logoMain.jpg"/>
                     <div className="header-information">
                         <h3>PresentOnline</h3>
                         <p>Магазин букетов</p>
@@ -20,8 +20,75 @@ function App() {
                 </ul>
             </header>
             <div className="content">
-                <h1>Все Букеты</h1>
-                +++++++++++++++++++++
+                <h1 className="content-title">Все Букеты</h1>
+                <div className="presents">
+                    <div className="card">
+                        <img width={133} height={112} src="/img/cart.svg" alt=""/>
+                        <p>Букет 1</p>
+                        <div className="card-info">
+                            <div className="card-info-price">
+                                <span>Цена:</span>
+                                <b>10000</b>
+                            </div>
+                            <button className="button">
+                                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img width={133} height={112} src="/img/cart.svg" alt=""/>
+                        <p>Букет 1</p>
+                        <div className="card-info">
+                            <div className="card-info-price">
+                                <span>Цена:</span>
+                                <b>10000</b>
+                            </div>
+                            <button className="button">
+                                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img width={133} height={112} src="/img/cart.svg" alt=""/>
+                        <p>Букет 1</p>
+                        <div className="card-info">
+                            <div className="card-info-price">
+                                <span>Цена:</span>
+                                <b>10000</b>
+                            </div>
+                            <button className="button">
+                                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img width={133} height={112} src="/img/cart.svg" alt=""/>
+                        <p>Букет 1</p>
+                        <div className="card-info">
+                            <div className="card-info-price">
+                                <span>Цена:</span>
+                                <b>10000</b>
+                            </div>
+                            <button className="button">
+                                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img width={133} height={112} src="/img/cart.svg" alt=""/>
+                        <p>Букет 1</p>
+                        <div className="card-info">
+                            <div className="card-info-price">
+                                <span>Цена:</span>
+                                <b>10000</b>
+                            </div>
+                            <button className="button">
+                                <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
