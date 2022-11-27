87c414069c7b671142f4e5aba8d3a8a75a2c8097
Yauhen Klimovich <webrubin1987@gmail.com>
2022-11-27 17:20:26 +0300

    feat: add logo and styles to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
-
 function App() {
     return (
         <div className="wrapper">
-
-
+            <header>
+                <div className="header-left-block">
+                    <img width={150} height={200} src="/img/logoMain.jpg"/>
+                    <div className="header-information">
+                        <h3>PresentOnline</h3>
+                        <p>Магазин букетов</p>
+                    </div>
+                </div>
+                <ul className="header-right-block">
+                    <li>
+                        <img width={20} height={20} src="/img/cart.svg"/>
+                        <span>1000</span>
+                    </li>
+                    <li>
+                        <img width={20} height={20} src="/img/cart.svg"/>
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>Все Букеты</h1>
+                +++++++++++++++++++++
+            </div>
         </div>
     );
 }
