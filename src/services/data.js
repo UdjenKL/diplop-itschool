40686060d0201f431550c6a6b213f29dcca5ea0b
Yauhen Klimovich <webrubin1987@gmail.com>
2022-12-01 11:17:48 +0300

    feat: edit data

--- src/services/data.js
+++ src/services/data.js
@@ -15,60 +15,60 @@ export const data = [
         "id": "3",
         "description": "Букет Фруктовый сад",
         "price": "100",
-        "imgSource": "img/Presents/3.jpg"
+        "imgSource": "img/presents/3.jpg"
     },
     {
         "id": "4",
         "description": "Букет Фруктовый сад 2",
         "price": "100",
-        "imgSource": "img/Presents/4.jpg"
+        "imgSource": "img/presents/4.jpg"
     },
     {
         "id": "5",
         "description": "Супер",
         "price": "120",
-        "imgSource": "img/Presents/5.jpg"
+        "imgSource": "img/presents/5.jpg"
     },
     {
         "id": "6",
         "description": "Букет шоколадный",
         "price": "100",
-        "imgSource": "img/Presents/6.jpg"
+        "imgSource": "img/presents/6.jpg"
     },
     {
         "id": "7",
         "description": "Букет Мужской",
         "price": "100",
-        "imgSource": "img/Presents/7.jpg"
+        "imgSource": "img/presents/7.jpg"
     },
     {
         "id": "8",
         "description": "Букет мужской",
         "price": "100",
-        "imgSource": "img/Presents/8.jpg"
+        "imgSource": "img/presents/8.jpg"
     },
     {
         "id": "9",
         "description": "Букет Фруктовый сад",
         "price": "100",
-        "imgSource": "img/Presents/9.jpg"
+        "imgSource": "img/presents/9.jpg"
     },
     {
         "id": "10",
         "description": "Букет Фруктовый сад",
         "price": "100",
-        "imgSource": "img/Presents/10.jpg"
+        "imgSource": "img/resents/10.jpg"
     },
     {
         "id": "11",
         "description": "Букет Мужской",
         "price": "100",
-        "imgSource": "img/Presents/11.jpg"
+        "imgSource": "img/presents/11.jpg"
     },
     {
         "id": "12",
         "description": "Букет Мужской",
         "price": "100",
-        "imgSource": "img/Presents/12.jpg"
+        "imgSource": "img/presents/12.jpg"
     }
 ]
