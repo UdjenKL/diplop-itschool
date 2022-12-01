0478cfb7d5e7c68f5680c5fbe1a044b72f8362a8
Yauhen Klimovich <webrubin1987@gmail.com>
2022-12-01 11:10:45 +0300

    feat: edit mockapi

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import {data} from './data.js';
 export const getItems = async (limit) => {
     try {
-        // const {data} = await axios.get(`https://6341bc1320f1f9d79978ecb3.mockapi.io/items/?page=1&limit=${limit}`)
+
+
+         // const {data} = await axios.get(`https://6341bc1320f1f9d79978ecb3.mockapi.io/items/?page=1&limit=${limit}`)
+
         return data
     } catch (error) {
         console.log(error);
